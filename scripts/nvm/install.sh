6490783d43cb6caabb4c37cdccb78b3c570d7fb0
Amo Wu <amo260@gmail.com>
2018-01-17 11:25:31 +0800

    feat(nvm): upgrade NVM to v0.33.5

--- scripts/nvm/install.sh
+++ scripts/nvm/install.sh
@@ -1,3 +1,3 @@
 if [ ! -d ~/.nvm ]; then
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
 fi
