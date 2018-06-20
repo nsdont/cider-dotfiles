29b9af2e7e0b5dd9ce37f0db5c6dea4dbc161908
Amo Wu <amo260@gmail.com>
2018-06-20 17:01:55 +0800

    feat(zsh): use spaceship theme

---
+++ scripts/spaceship/path.zsh
@@ -0,0 +1,23 @@
+# ORDER
+SPACESHIP_PROMPT_ORDER=(
+  time
+  user
+  host
+  dir
+  git
+  node
+  aws
+  pyenv
+  exec_time
+  line_sep
+  vi_mode
+  jobs
+  exit_code
+  char
+)
+
+# PROMPT
+SPACESHIP_PROMPT_SYMBOL="$"
+
+# TIME
+SPACESHIP_TIME_SHOW=true
