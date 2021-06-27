f7f4e148487eba93a4a91f09067a833f649c7758
NSDont <haodewanan@gmail.com>
2021-06-27 16:22:02 +0800

    feat(zsh): add new plugin git-open

--- scripts/zplug/plug.zsh
+++ scripts/zplug/plug.zsh
@@ -2,5 +2,6 @@ zplug "supercrabtree/k"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "sukkaw/zsh-osx-autoproxy"
 zplug "zsh-users/zsh-syntax-highlighting"
+zplug "paulirish/git-open", as:plugin
 
 zplug load
